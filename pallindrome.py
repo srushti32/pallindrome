e5d2196b0a1f402eda132a499522b99eaab1f2c7
srushti32 <hsrushti320@gmail.com>
2025-11-28 15:49:32 +0530

    Update pallindrome.py

--- pallindrome.py
+++ pallindrome.py
@@ -1,12 +1,13 @@
 import sys
 
-if len(sys.argv) ! = 2:
-print("usage: python pallindrome.py <string>")
-sys.exit(1)
+if len(sys.argv) != 2:
+    print("usage: python palindrome.py <string>")
+    sys.exit(1)
 
 string = sys.argv[1]
 
-if string == string[:: 2]:
-  print(f"{string} is a palindrome")
-  else
-  print(f"{string} is not a palindrome")
+if string == string[::-1]:
+    print(f"{string} is a palindrome")
+else:
+    print(f"{string} is not a palindrome")
+
